3641be0aacf5648ab2e3b247d784441200cf450f
Schecter123 <53582747+Schecter123@users.noreply.github.com>
2020-11-20 10:26:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,136 +3,125 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/chmura";
 
-    static final String USER = "JRadomski";
-    static final String PASS = "root";
-
-    static Scanner in = new Scanner( System.in);
-    private static final String CREATE_TABLE_USERS = "CREATE TABLE IF NOT EXISTS Users (ID int, NAME varchar(255), SURNAME varchar(255), YEAR varchar(255), TYPE varchar(255) );";
-    private static final String SELECT_ALL_FROM_USERS = "SELECT ID, NAME, SURNAME, YEAR, TYPE FROM Users";
+    static Scanner in = new Scanner(System.in);
+    private static final String SELECT_ALL_FROM_CARS = "SELECT ID, BRAND, MODEL, PRODUCTION_YEAR, TYPE FROM Cars";
 
     public static void main(String[] args) {
 
-        try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            Statement stmt = conn.createStatement()) {
-            Class.forName("com.mysql.jdbc.Driver");
-            TimeUnit.SECONDS.sleep(10);
-            System.out.println("Connecting to database...");
-            stmt.executeUpdate(CREATE_TABLE_USERS);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/chmuraTest", "JRadomski", "root");
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Students (ID int NOT NULL AUTO_INCREMENT, Imie varchar(255), Nazwisko varchar(255), Miasto varchar(255), Indeks integer(255), PRIMARY KEY (ID) );");
             String selectedOperation;
-            do
-            {
-                System.out.println("1. Show DB data\n2. Insert user\n3. Edit user by ID\n4. Delete user by ID\nS. Press E to Exit");
+            do {
+                System.out.println("1. Pokaż dane bazy\n2. Dodaj studenta\n3. Edytuj studenta\n4. Usuń studenta\nS. Naciśnij E by wyjść");
                 selectedOperation = in.nextLine();
-                switch( selectedOperation )
-                {
-                    case "1" :
+                switch (selectedOperation) {
+                    case "1":
                         getResults(stmt);
                         break;
-                    case "2" :
-                        insertUser(stmt);
+                    case "2":
+                        insertStudent(stmt);
                         break;
-                    case "3" :
-                        updateUser(stmt);
+                    case "3":
+                        updateStudent(stmt);
                         break;
-                    case "4" :
-                        deleteUserById(stmt);
+                    case "4":
+                        deleteStudent(stmt);
                         break;
                 }
-            }while (!selectedOperation.toUpperCase().equals("E"));
-        } catch (InterruptedException | SQLException | ClassNotFoundException e) {
+            } while (!selectedOperation.toUpperCase().equals("E"));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void deleteUserById(Statement stmt) throws SQLException {
-        ResultSet rsss = stmt.executeQuery(SELECT_ALL_FROM_USERS);
+    private static void deleteStudent(Statement stmt) throws SQLException {
+        ResultSet rsss = stmt.executeQuery("SELECT ID, Imie, Nazwisko, Miasto, Indeks FROM Students");
         printOutHeader();
         printOutResult(rsss);
         rsss.close();
-        System.out.println("Enter ID to delete");
+        System.out.println("Podaj ID studenta, który chcesz usunąć");
         final String id = in.nextLine();
-        final String deleteSql = " DELETE FROM Users WHERE ID= '"+id+"';";
+        final String deleteSql = " DELETE FROM Students WHERE ID= '" + id + "';";
         stmt.executeUpdate(deleteSql);
     }
 
-    private static void updateUser(Statement stmt) throws SQLException {
-        String type;
+    private static void updateStudent(Statement stmt) throws SQLException {
         String id;
-        String name;
-        String surname;
+        String imie;
+        String nazwisko;
+        String miaso;
+        int indeks;
         String sql;
-        String year;
-        ResultSet rss = stmt.executeQuery(SELECT_ALL_FROM_USERS);
+
+        ResultSet rss = stmt.executeQuery("SELECT ID, Imie, Nazwisko, Miasto, Indeks FROM Students");
         printOutHeader();
 
         printOutResult(rss);
         rss.close();
-        System.out.println("Enter ID to edit");
+        System.out.println("Podaj ID studenta, którego chcesz edytować");
         id = in.nextLine();
 
-        System.out.println("Name: ");
-        name = in.nextLine();
+        System.out.println("Imie: ");
+        imie = in.nextLine();
 
-        System.out.println("Surname: ");
-        surname = in.nextLine();
+        System.out.println("Nazwisko: ");
+        nazwisko = in.nextLine();
 
-        System.out.println("Year:");
-        year = in.nextLine();
+        System.out.println("Miasto:");
+        miaso = in.nextLine();
 
-        System.out.println("Type");
-        type = in.nextLine();
-        sql = " UPDATE Users SET NAME = '"+name+"' , SURNAME = '"+surname+"', YEAR = '"+year+"',TYPE ='"+type+"' WHERE ID= '"+id+"';";
+        System.out.println("Numer indeksu");
+        indeks = in.nextInt();
+        sql = " UPDATE Students SET Imie = '" + imie + "' , Nazwisko = '" + nazwisko + "', Miasto = '" + miaso + "',Indeks ='" + indeks + "' WHERE ID= '" + id + "';";
         stmt.executeUpdate(sql);
     }
 
-    private static void insertUser(Statement stmt) throws SQLException {
-        System.out.println("ID");
-        final String id = in.nextLine();
+    private static void insertStudent(Statement stmt) throws SQLException {
 
-        System.out.println("Name:");
-        final String name = in.nextLine();
 
-        System.out.println("Surname");
-        final String surname = in.nextLine();
+        System.out.println("Imie:");
+        final String imie = in.nextLine();
 
-        System.out.println("Year:");
-        final String year = in.nextLine();
+        System.out.println("Nazwisko");
+        final String nazwisko = in.nextLine();
 
-        System.out.println("Type");
-        final String type = in.nextLine();
-        
-        String sql = " INSERT INTO Users (ID, NAME, SURNAME, YEAR,TYPE) VALUES ('"+id+"', '"+name+"', '"+surname+"', '"+year+"','"+type+"')";
+        System.out.println("Miasto");
+        final String miasto = in.nextLine();
+
+        System.out.println("Numer indeksu");
+        final int indeks = in.nextInt();
+
+        String sql = " INSERT INTO Students (Imie, Nazwisko, Miasto, Indeks) VALUES ('" + imie + "', '" + nazwisko + "', '" + miasto + "','" + indeks + "')";
         stmt.executeUpdate(sql);
     }
 
     private static void getResults(Statement stmt) throws SQLException {
-        ResultSet rs = stmt.executeQuery(SELECT_ALL_FROM_USERS);
+        ResultSet rs = stmt.executeQuery("SELECT ID, Imie, Nazwisko, Miasto, Indeks FROM Students");
         printOutHeader();
         printOutResult(rs);
         rs.close();
     }
 
     private static void printOutHeader() {
-        System.out.println("ID    NAME    SURNAME    YEAR    TYPE");
+        System.out.println("ID    Imie    Nazwisko    Miasto    Indeks");
     }
 
     private static void printOutResult(ResultSet rs) throws SQLException {
         int id;
-        String first;
-        String last;
-        String address;
-        String city;
+        String imie;
+        String nazwisko;
+        String miasto;
+        int indeks;
         while (rs.next()) {
             id = rs.getInt("ID");
-            first = rs.getString("NAME");
-            last = rs.getString("SURNAME");
-            address = rs.getString("YEAR");
-            city = rs.getString("TYPE");
+            imie = rs.getString("Imie");
+            nazwisko = rs.getString("Nazwisko");
+            miasto = rs.getString("Miasto");
+            indeks = rs.getInt("Indeks");
 
-            System.out.println(id + "    " + first + "    " + last + "    " + address + "    " + city);
+            System.out.println(id + "    " + imie + "    " + nazwisko + "    " + miasto + "    " + indeks);
         }
     }
 }
